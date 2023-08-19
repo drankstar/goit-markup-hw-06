9d51c23aff1e836e3acca5679d1f469e18a53802
drankstar <thedrankstar@gmail.com>
2023-08-19 12:51:36 +0300

    hw4

---
+++ js/mob-menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openmenuBtn: document.querySelector("[data-menu-open]"),
+    closemenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openmenuBtn.addEventListener("click", togglemenu);
+  refs.closemenuBtn.addEventListener("click", togglemenu);
+
+  function togglemenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
